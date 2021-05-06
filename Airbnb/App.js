d1184fb952cb16be855f09c78f28853c43a68c39
parkoon <devparkoon@gmail.com>
2021-05-06 14:53:31 +0900

    feat: post components

--- Airbnb/App.js
+++ Airbnb/App.js
@@ -3,6 +3,9 @@ import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import HomeScreen from './src/screens/Home';
+import Post from './src/components/Post';
+
+import places from './assets/data/feed';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -11,11 +14,14 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const post = places[0];
+
   return (
     <>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <SafeAreaView style={backgroundStyle}>
-        <HomeScreen />
+        {/* <HomeScreen /> */}
+        <Post post={post} />
       </SafeAreaView>
     </>
   );
